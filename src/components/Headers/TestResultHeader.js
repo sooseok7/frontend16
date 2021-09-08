8f9590bd6caf84843c38e6abcb3f5d45148be413
joo-yongjae <0443jun@gmail.com>
2021-09-08 13:28:30 +0900

    검색창 ui 및 테스트 결과 페이지 헤더 생성

---
+++ src/components/Headers/TestResultHeader.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+// reactstrap components
+import {Container } from "reactstrap";
+
+// core components
+
+function TestPageHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+  return (
+    <>
+      <div className="page-header">
+        <div
+          className="page-header-image"
+          style={{
+            backgroundImage:
+              "url(" + require("assets/img/bg1.jpg").default + ")",
+          }}
+          ref={pageHeader}
+        ></div>
+        <div className="content-center">
+          <Container>
+            <h1 className="title">여행성향 MBTI 테스트 결과</h1>
+          </Container>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default TestPageHeader;
