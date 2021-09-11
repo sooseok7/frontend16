56dee7f8663bcf5eb6a08f33d363a48b73f320a4
joo-yongjae <0443jun@gmail.com>
2021-09-11 20:06:15 +0900

    받으셈

--- src/Anyday.js
+++ src/Anyday.js
@@ -1,4 +1,4 @@
-const Holiday =  [
+const Anyday =  [
     {
       id: 0,
       title:"출근",
@@ -10,9 +10,9 @@ const Holiday =  [
       id: 1,
       title:"퇴근",
       allDat:false,
-      start: new Date(2021,11,9,10,0),
-      end: new Date(2021,11,9,10,30)
+      start: new Date(2021,9,9,10,0),
+      end: new Date(2021,9,9,10,30)
     }
   ];
  
-export default Holiday;
+export default Anyday;
