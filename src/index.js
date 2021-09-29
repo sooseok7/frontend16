2057b35aefd81d9956428ceeb50f91bce9a5d8b9
hahayoungyou <whgkdud0501@naver.com>
2021-09-29 17:36:02 +0900

    ?

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 // styles for this kit
 import "assets/css/bootstrap.min.css";
 import "assets/scss/now-ui-kit.scss?v=1.5.0";
-import "assets/demo/demo.css?v=1.5.0";
-import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
+// import "assets/demo/demo.css?v=1.5.0";
+// import "assets/demo/nucleo-icons-page-styles.css?v=1.5.0";
 // pages for this kit
 import Index from "views/Index.js";
 import NucleoIcons from "views/NucleoIcons.js";
